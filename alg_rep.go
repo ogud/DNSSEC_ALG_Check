2fa0859afb607c2d8a8ccd26ce5149499ffcd7e0
Olafur Gudmundsson <ogud@ogud.com>
2015-02-16 09:07:33 -0500

    Added names of algorithms to each output line Added explicit check if address is a resolver Simplified the timout possible to extend via command line flag (not yet done)

--- alg_rep.go
+++ alg_rep.go
@@ -7,7 +7,7 @@
  *                  -v verbose
  *                  -d debug
  */
-/* Output codes 
+/* Output codes for each question 
  *     V  answer verified ==? algorithm supported 
  *     -  unverified answer 
  *     x  Unspecified algorithm/digest/negative answer combination 
@@ -20,20 +20,22 @@ package main
 import (
 	"flag"
 	"fmt"
+	"os"
 	"github.com/miekg/dns"
 )
 // change the array sizes below when new algorithms are added
 // When new algorithms are added the function print_table() needs updating
-// List the defined signature algorithms as off 2014/11
+// List the defined signature algorithms as of 2014/11
 // 2 4 9 11 skipped as not valid algs 14 is max 
-/*var algs = []string{"alg-1-nsec", "alg-3-nsec", "alg-5-nsec",
-	"alg-6-nsec", "alg-7-nsec", "alg-8-nsec", "alg-10-nsec",
-	"alg-12-nsec", "alg-13-nsec", "alg-14-nsec"}
-*/
+
 const maxAlg = 10  
 const maxDs = 4
 var algs = [maxAlg] string { "alg-1", "alg-3", "alg-5", "alg-6", "alg-7", "alg-8", 
 	"alg-10", "alg-12", "alg-13", "alg-14" }
+var names = [maxAlg] string { "RSAMD5", "DSA", "RSASHA1", "DSA-NSEC3-SHA1", 
+	"RSASHA1-NSEC3",
+	"RSASHA-256", "RSASHA-512", "ECC-GOST", "ECSDAP256-SHA256", "ECSDAP384-SHA384"}
+
 // List the define DS digiest alogrithms As of 2014/11
 var ds = [maxDs]string{"ds-1", "ds-2", "ds-3", "ds-4"}
 
@@ -42,16 +44,15 @@ var number = [10] string { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9" }
 var result[maxAlg][maxDs + maxDs ] string // Results are stored in here 
 var zone string = "dnssec-test.org."  // Our test zone anchors
 var debug bool = false
+var maxRetry = 1
 
 // Work sets up one go-routine for each Digest algorithm 
 func work( d int, myType uint16, resolver string, verb bool, done chan bool) {
 	// Scan the digest algorithms for both NSEC and NSEC3 zones
 	for a := range algs {
-		name := algs[a] + "-nsec"
-		result[a][d]  = supports(ds[d]+ "." + name + "." + zone, 
-			myType, resolver, verb)
-		result[a][d + maxDs] = supports(ds[d] + "." + name + "3." + zone, 
-			myType, resolver, verb)
+		name := ds[d] + "." + algs[a] + "-nsec"
+		result[a][d]  = supports(name + "." + zone, myType, resolver, verb)
+		result[a][d + maxDs] = supports(name + "3." + zone, myType, resolver, verb)
 	}
 	done <- true // report completion
 }
@@ -67,13 +68,18 @@ func main() {
 	verbose := flag.Bool("v", false, "Short output")
 	flag.Parse()
 	// Extract supplied parameters
-	myType := uint16(48)  // DNSKEY GetType(qtype)
+	myType := uint16(48)  // DNSKEY (as single key signed zone this is suffiicent test
 	debug = *deb
 	myRes := *resolver
-	// need 
-	// this also does priming query to get dnssec-test.org into cache and check if 
-	// resolver valiates (no need to query if not validating)
+
+	// first do a priming query to get dnssec-test.org into cache and check if 
+	// resolver valiates ==> no need to continue if resolver is not validating
 	prime := supports(zone, myType, myRes, true)
+	if (prime == " T ") {
+		fmt.Printf("Not a resolver ???  %s\n", myRes)
+		os.Exit(1)
+	} 
+
 	fmt.Printf("Zone %s  Qtype DNSKEY Resolver %s debug=%v verbose=%v Prime=%v\n", zone, 
 		myRes, debug, *verbose, prime)
 	if (prime == " V ") { // priming we can check all algorithms 
@@ -125,7 +131,8 @@ func print_table() {
 		if len(msg) < 6 {
 			msg += " "
 		}
-		fmt.Printf("%s\n", msg + " : " + list_supp(result[a]))
+		fmt.Printf("%s\n", msg + " : " + list_supp(result[a]) + " => " +
+		names[a])
 	}
 	fmt.Printf( "V == Validates  - == Answer  x == Alg Not specified\n" + 
 		   "T == Timeout S == ServFail O == Other Error\n" + 
@@ -144,36 +151,41 @@ func PrintSection( prefix string, z [] dns.RR, display bool) {
 	}
 }
 
-
+// function to perform the lookup and retries
+// returns the message and a flag if there was a timeout
+// 
 func doLookup( qn string, qt uint16, resolver string)  (*dns.Msg, bool) {
+	var err error
 	c := dns.Client{}
 	m := &dns.Msg{}
 	m.SetEdns0(2048, true)  // asking for DO bit  // create a fallback XXX
 	m.SetQuestion(qn, qt)
 	// need to set a longer timerout
 
-	msg, _, err := c.Exchange(m, resolver + ":53")
-	if err != nil {  // most likely timeout retry
-		msg, _, err = c.Exchange(m, resolver + ":53")
-		if (err != nil) {
-			fmt.Printf("Lookup Error %s %v\n", qn, err)
-			return msg, true
+	for i := 0; i <= maxRetry; i++ {
+		msg, _, err := c.Exchange(m, resolver + ":53")
+		if  err == nil {
+			return msg, false
 		}
 	}
-	return msg, false
+	fmt.Printf("Lookup Error %s %v\n", qn, err)
+	return nil, true
 }
 
 /* 
  * A function to ask a question and returns if the answer existed and if it was validated
  * Input is the name and query type, what resolver to use and if debugging is turned on
  */
-func validate_name( qn string, qt uint16, resolver string, debug bool) (supp string, msg string){
+func validate_name( qn string, qt uint16, resolver string, debug bool) (supp string, 
+	msg string) {
+
 	name, timeout := doLookup(qn, qt, resolver) 
 	if (timeout)  {
-		return " T ", "Lookup Errorr"
+		return " T ", "Lookup Error"
 	}
 	supp = " - "
-	counts := number[len(name.Answer)] + " " + number[len(name.Ns)] + " " + number[len(name.Extra)]
+	counts := number[len(name.Answer)] + " " + number[len(name.Ns)] + " " + 
+		number[len(name.Extra)]
 	switch {
 	case name.Rcode == dns.RcodeSuccess && len(name.Answer) > 0:
 		// got an actual answer 
